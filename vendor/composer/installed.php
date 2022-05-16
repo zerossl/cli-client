08611913211dc629797c6f40337eabaff83eabd4
David Stöckl <david.stoeckl@blackbam.at>
2022-05-16 17:05:09 +0200

    fix. commit autoloading in order to remove composer as a dependency

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5666a489ac36066b714e444dbca9e788759e0881',
+    'name' => 'zerossl/cli-client',
+  ),
+  'versions' => 
+  array (
+    'zerossl/cli-client' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5666a489ac36066b714e444dbca9e788759e0881',
+    ),
+  ),
+);
